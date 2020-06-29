d9d60dd4b99fe3fda8b7d8b8078203a8889dc3f5
eternalarchive <unchd26@gmail.com>
2020-06-29 23:25:14 +0900

    bug fix: header date

--- src/components/Header/NowTime.tsx
+++ src/components/Header/NowTime.tsx
@@ -5,7 +5,7 @@ import * as S from './HeaderStyle';
 function NowTime() {
   return (
     <div>
-      <S.NowTimeText>{moment().format('MMM Do YY').slice(0, 7)}</S.NowTimeText>
+      <S.NowTimeText>{moment().format('MMM Do YY').slice(0, 8)}</S.NowTimeText>
       <S.NowTimeText>{moment().format('LT')}</S.NowTimeText>
     </div>
   );
