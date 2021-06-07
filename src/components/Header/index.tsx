8f1580ef98ebd945eed4efa6be656c1fb18425e0
DAHEE JEONG <unchd26@gmail.com>
2021-06-07 18:00:14 +0900

    refactor: 왼쪽 상단 타이틀 클릭시 홈으로, 영어 입력 방지 안내 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import * as S from './HeaderStyle';
 import NowTime from './NowTime';
 import ModeContainer from '../../containers/ModeContainer';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <S.HeaderBox>
-      <S.HeaderTitle>Blue Weather</S.HeaderTitle>
+      <Link to="/">
+        <S.HeaderTitle>Blue Weather</S.HeaderTitle>
+      </Link>
       <NowTime />
       <ModeContainer />
     </S.HeaderBox>
